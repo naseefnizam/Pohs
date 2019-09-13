33277298dad1cd92a036c33687173e75bedfd053
mac <vivekjeevanarajh@gmail.com>
2019-09-13 23:22:42 +0530

    Edition 2

--- app/src/main/java/com/example/pohs/DashBoard/MainActivity.java
+++ app/src/main/java/com/example/pohs/DashBoard/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         mgetstartBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-             Intent intent = new Intent(MainActivity.this,ShopDashBoard.class);
+             Intent intent = new Intent(MainActivity.this,CustomerDashBoard.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/pohs/DashBoard/MaleItems.java
+++ app/src/main/java/com/example/pohs/DashBoard/MaleItems.java
@@ -6,6 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
 
 import com.example.pohs.R;
 
@@ -20,12 +27,66 @@ public class MaleItems extends Fragment {
         // Required empty public constructor
     }
 
+    final int maleItemImg[] = {R.drawable.man, R.drawable.menaaa, R.drawable.menm,R.drawable.mens, R.drawable.menaaa, R.drawable.mens};
+
+    final String MaleItemName[] ={"Dress1","Dress2","Dress3","Dress4","Dress5","Dress6"};
+
+    final String MaleItemSize[] = {"23","24","34","32","23","31"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_male_items, container, false);
+
+        final View  views = inflater.inflate(R.layout.fragment_male_items,container,false);
+
+
+
+        ListView listView = (ListView)views.findViewById(R.id.MaleItemLV);
+
+        MaleItemAdepter maleItemAdepter = new MaleItemAdepter();
+
+        listView.setAdapter(maleItemAdepter);
+
+        return views;
     }
+
+    class MaleItemAdepter extends BaseAdapter{
+
+
+
+        @Override
+        public int getCount() {
+            return maleItemImg.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            View view = getLayoutInflater().inflate(R.layout.custom_male_item_view,null);
+
+            ImageView imageView = (ImageView)view.findViewById(R.id.miimageView);
+            TextView textView_miName = (TextView)view.findViewById(R.id.textView_miname);
+            TextView textView_miSize = (TextView)view.findViewById(R.id.textView_misize);
+
+            imageView.setImageResource(maleItemImg[position]);
+            textView_miName.setText(MaleItemName[position]);
+            textView_miSize.setText(MaleItemSize[position]);
+
+            return view ;
+        }
+    }
+
+
 
 }
