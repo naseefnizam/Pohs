78576eab9d81deea06e02f4699c996471c7385de
mac <vivekjeevanarajh@gmail.com>
2019-09-14 14:30:42 +0530

    with image Button

--- app/src/main/java/com/example/pohs/DashBoard/Upload.java
+++ app/src/main/java/com/example/pohs/DashBoard/Upload.java
@@ -1,10 +1,13 @@
 package com.example.pohs.DashBoard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 
 import com.example.pohs.R;
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
@@ -14,6 +17,10 @@ public class Upload extends AppCompatActivity {
     private Toolbar mtoolbar;
     String[] SPINNERLIST ={"Male Item","Female Item","Kids Item"};
 
+    ImageButton imageButton ;
+    private int Gallary_intent =2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +29,9 @@ public class Upload extends AppCompatActivity {
         mtoolbar = (Toolbar)findViewById(R.id.Upload_dashboard_toolbar);
         //setSupportActionBar(mtoolbar);
         getSupportActionBar().setTitle("UPLOAD");
+
+
+        imageButton = (ImageButton)findViewById(R.id.imgbtn);
 
         //dropdown for dress type
 
@@ -36,5 +46,11 @@ public class Upload extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main_menu,menu);
 
         return true;
+    }
+
+    public void imgbutton(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,Gallary_intent);
     }
 }
